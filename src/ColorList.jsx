3b71ce1f9bd076fa0496c4e6f9b9a466bd2d18b3
Lawrence Caulker <locaulker@gmail.com>
2023-09-26 20:49:14 -0400

    implemented color list

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,4 +1,13 @@
-const ColorList = () => {
-  return <div>ColorList</div>
+import SingleColor from "./SingleColor"
+import { nanoid } from "nanoid"
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />
+      })}
+    </section>
+  )
 }
 export default ColorList
