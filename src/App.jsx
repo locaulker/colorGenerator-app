50996e16eb555c0210a7de510197fdb8d682057f
Lawrence Caulker <locaulker@gmail.com>
2023-09-27 00:30:12 -0400

    installed and setup toastify

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ColorList from "./ColorList"
 import Form from "./Form"
 import Values from "values.js"
 
+import { ToastContainer, toast } from "react-toastify"
+
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10))
 
@@ -10,6 +12,7 @@ const App = () => {
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   )
 }
